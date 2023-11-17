9164097c1bad69377c8effcbda810f522966025c
Orrv2904 <Omarrv2904@gmail.com>
2023-11-17 16:03:22 -0600

    :construction:

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -23,11 +27,64 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(e) {
+  e.preventDefault();
+  const recipient = document.querySelector('#compose-recipients').value
+  const subject = document.querySelector('#compose-subject').value
+  const body = document.querySelector('#compose-body').value
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipient,
+      subject: subject,
+      body: body
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+
+      if (result && result.error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: result.error,
+        });
+      } else if (result && result.message) {
+        Swal.fire({
+          icon: 'info',
+          title: 'Info',
+          text: result.message,
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'An unexpected error occurred.',
+        });
+      }
+
+      load_mailbox('sent');
+    })
+    .catch(error => {
+      console.error(error);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'An error occurred while processing your request.',
+      });
+
+    });
+
+
 }
